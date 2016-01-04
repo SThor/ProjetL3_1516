3f11e578b765c64537754b3b497a336ebcf3b98e
SThor <paulgivel@gmail.com>
2016-01-04 13:33:47 +0100

    Teleportation

--- src/serveur/interaction/Teleportation.java
+++ src/serveur/interaction/Teleportation.java
@@ -1,5 +1,80 @@
 package serveur.interaction;
 
-public class Teleportation extends Interaction<T> {
+import java.awt.Point;
+import java.rmi.RemoteException;
+import java.util.HashMap;
 
+import serveur.vuelement.VuePersonnage;
+import utilitaires.Calculs;
+
+public class Teleportation {
+	/**
+	 * Vue du personnage qui veut se deplacer.
+	 */
+	private VuePersonnage personnage;
+	
+	/**
+	 * References RMI et vues des voisins (calcule au prealable). 
+	 */
+	private HashMap<Integer, Point> voisins;
+	
+	/**
+	 * Cree un deplacement.
+	 * @param personnage personnage voulant se deplacer
+	 * @param voisins voisins du personnage
+	 */
+	public Teleportation(VuePersonnage personnage, HashMap<Integer, Point> voisins) { 
+		this.personnage = personnage;
+
+		if (voisins == null) {
+			this.voisins = new HashMap<Integer, Point>();
+		} else {
+			this.voisins = voisins;
+		}
+	}
+
+	/**
+	 * Deplace ce sujet sur l'element dont la reference
+	 * est donnee.
+	 * Si la reference est la reference de l'element courant, il ne bouge pas ;
+	 * si la reference est egale a 0, il se teleporte a un endroit aleatoire ;
+	 * sinon il va sur le voisin correspondant (s'il existe dans les voisins).
+	 * @param refObjectif reference de l'element cible
+	 */    
+	public void seTeleporteA(int refObjectif) throws RemoteException {
+		Point pvers;
+
+		// on ne bouge que si la reference n'est pas la notre
+		if (refObjectif != personnage.getRefRMI()) {
+			
+			// la reference est nulle (en fait, nulle ou negative) : 
+			// le personnage erre
+			if (refObjectif <= 0) { 
+				pvers = Calculs.positionAleatoireArene();
+						
+			} else { 
+				// sinon :
+				// la cible devient le point sur lequel se trouve l'element objectif
+				pvers = voisins.get(refObjectif);
+			}
+	
+			// on ne bouge que si l'element existe
+			if(pvers != null) {
+				seDirigeVers(pvers);
+			}
+		}
+	}
+
+	/**
+	 * Deplace ce sujet sur la case donnee.
+	 * @param objectif case cible
+	 * @throws RemoteException
+	 */
+	public void seDirigeVers(Point objectif) throws RemoteException {
+		Point cible = Calculs.restreintPositionArene(objectif); 
+		
+		if(cible != null) {
+			personnage.setPosition(cible);
+		}
+	}
 }

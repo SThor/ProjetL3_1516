8c159f4b0b7eea6a9ae3cf7d13b988dabef796b0
jimpetiot <jimpetiot@130.120.210.28>
2016-01-04 13:47:50 +0100

    AttaqueDistance

--- src/serveur/interaction/AttaqueDistance.java
+++ src/serveur/interaction/AttaqueDistance.java
@@ -1,5 +1,75 @@
 package serveur.interaction;
 
-public class AttaqueDistance extends Interaction<T> {
+import java.awt.Point;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+
+import serveur.Arene;
+import serveur.element.Caracteristique;
+import serveur.element.Personnage;
+import serveur.vuelement.VuePersonnage;
+import utilitaires.Calculs;
+import utilitaires.Constantes;
+
+public class AttaqueDistance extends Interaction<VuePersonnage> {
+
+	/**
+	 * Cree une interaction d'attaque ˆ distance.
+	 * @param arene arene
+	 * @param attaquant attaquant
+	 * @param defenseur defenseur
+	 */
+	public AttaqueDistance(Arene arene, VuePersonnage attaquant, VuePersonnage defenseur) {
+		super(arene, attaquant, defenseur);
+	}
+
+	@Override
+	public void interagit() {
+		try {
+			Personnage pAttaquant = attaquant.getElement();
+			int forceAttaquant = pAttaquant.getCaract(Caracteristique.FORCE);
+			/**
+			 * la perte de vie est Žgale a la force de l'attaquant divisee par 2, une attaque a distance
+			 * etant moins puissante qu'une attaque au corps a corps
+			 */
+			int perteVie = (forceAttaquant /2) ;
+		
+			// degats
+			if (perteVie > 0) {
+				arene.incrementeCaractElement(defenseur, Caracteristique.VIE, -perteVie);
+				
+				logs(Level.INFO, Constantes.nomRaccourciClient(attaquant) + " colle une beigne ("
+						+ perteVie + " points de degats) a " + Constantes.nomRaccourciClient(defenseur));
+			}
+			
+			// initiative
+			incrementeInitiative(defenseur);
+			decrementeInitiative(attaquant);
+			
+		} catch (RemoteException e) {
+			logs(Level.INFO, "\nErreur lors d'une attaque : " + e.toString());
+		}
+		
+	}
+	
+	/**
+	 * Incremente l'initiative du defenseur en cas de succes de l'attaque. 
+	 * @param defenseur defenseur
+	 * @throws RemoteException
+	 */
+	private void incrementeInitiative(VuePersonnage defenseur) throws RemoteException {
+		arene.incrementeCaractElement(defenseur, Caracteristique.INITIATIVE, 
+				Constantes.INCR_DECR_INITIATIVE_DUEL);
+	}
+	
+	/**
+	 * Decremente l'initiative de l'attaquant en cas de succes de l'attaque. 
+	 * @param attaquant attaquant
+	 * @throws RemoteException
+	 */
+	private void decrementeInitiative(VuePersonnage attaquant) throws RemoteException {
+		arene.incrementeCaractElement(attaquant, Caracteristique.INITIATIVE, 
+				-Constantes.INCR_DECR_INITIATIVE_DUEL);
+	}
 
 }

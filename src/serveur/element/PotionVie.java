9ed2756ba7cfde4957f558f2a2976573a8c65798
Jason <Jason.cabanac@laposte.net>
2016-01-04 14:57:19 +0100

    test

--- src/serveur/element/PotionVie.java
+++ src/serveur/element/PotionVie.java
@@ -1,5 +1,13 @@
 package serveur.element;
 
+import java.util.HashMap;
+
 public class PotionVie extends Potion {
+
+	public PotionVie(String nom, String groupe,
+			HashMap<Caracteristique, Integer> caracts) {
+		super(nom, groupe, caracts);
+		// TODO Auto-generated constructor stub
+	}
 
 }

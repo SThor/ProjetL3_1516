232cd637a0da1c0ade37e992bfbe42d0c52eecdd
Jason <Jason@Jasonator>
2016-01-05 16:32:22 +0100

    StratBourrin (non testée)

--- src/client/strategie/StratBourrin.java
+++ src/client/strategie/StratBourrin.java
@@ -3,17 +3,25 @@ package client.strategie;
 import java.awt.Point;
 import java.rmi.RemoteException;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import logger.LoggerProjet;
 import serveur.IArene;
 import serveur.element.Caracteristique;
+import serveur.element.Element;
+import serveur.element.Personnage;
 import utilitaires.Calculs;
+import utilitaires.Constantes;
 
+/* 
+ * Cette classe met en place le comportement de type "Fuyard".
+ * Celui-ci fuit les combats même si il pouvait les gagner en sélectionnant parmis les directions celle qui l'éloigne le plus de tous les ennemis en vue
+ */
 public class StratBourrin extends StrategiePersonnage {
 
-	public StratBourrin(	String ipArene, int port, String ipConsole, String nom,
-							String groupe, HashMap<Caracteristique, Integer> caracts,
-							int nbTours, Point position, LoggerProjet logger) 
+	public StratBourrin	(String ipArene, int port, String ipConsole, String nom,
+			String groupe, HashMap<Caracteristique, Integer> caracts,
+			int nbTours, Point position, LoggerProjet logger) 
 	{
 		super(ipArene, port, ipConsole, nom, groupe, caracts, nbTours, position, logger);
 	}
@@ -42,38 +50,58 @@ public class StratBourrin extends StrategiePersonnage {
 			arene.deplace(refRMI, 0); 
 
 		} else {
-			console.setPhrase("Je charge !");
-			arene.deplace(refRMI, trouverPointFuite(position, voisins) );
+			int refIlemPlusProche = Calculs.chercheElementProche(position, voisins);
+			Element elemPlusProche = arene.elementFromRef(refIlemPlusProche);
+			if(elemPlusProche instanceof Personnage){
+				if(Calculs.distanceChebyshev(position, arene.getPosition(refIlemPlusProche))< Constantes.DISTANCE_MIN_INTERACTION)
+				{
+					console.setPhrase("Je fais un duel avec " + elemPlusProche.getNom());
+					arene.lanceAttaque(refRMI, refIlemPlusProche);
+				}
+			}
+			else{
+				console.setPhrase("Je charge !");
+				arene.deplace(refRMI, trouverCible(position, voisins) );
+			}
 		}
 
 	}
 
-	private Point trouverPointFuite(Point position, HashMap<Integer, Point> voisins)
+	private Point trouverCible(Point position, HashMap<Integer, Point> voisins) throws RemoteException 
 	{
-		Point[] possibilites = new Point[] { new Point((int)position.getX() +1 , (int)position.getY() + 1),
-				new Point((int)position.getX() +1 , (int)position.getY()    ),
-				new Point((int)position.getX() +1 , (int)position.getY() - 1),
-				new Point((int)position.getX()    , (int)position.getY() + 1),
-				new Point((int)position.getX()    , (int)position.getY() - 1),
-				new Point((int)position.getX() -1 , (int)position.getY() + 1),
+		IArene arene = console.getArene();
+		Point[] possibilites = new Point[] { 
+				new Point((int)position.getX() -1 , (int)position.getY() - 1),
 				new Point((int)position.getX() -1 , (int)position.getY()    ),
-				new Point((int)position.getX() -1 , (int)position.getY() - 1)
+				new Point((int)position.getX() -1 , (int)position.getY() + 1),
+				new Point((int)position.getX()    , (int)position.getY() - 1),
+				new Point((int)position.getX()    , (int)position.getY() + 1),
+				new Point((int)position.getX() +1 , (int)position.getY() - 1),
+				new Point((int)position.getX() +1 , (int)position.getY()    ),
+				new Point((int)position.getX() +1 , (int)position.getY() + 1)
 		};
-		double meilleureMoyenneDistance = -1;
-		int meilleurPoint = -1;
-		for(int j = 0; j< possibilites.length; j++){
-			if(Calculs.estDansArene(possibilites[j])){
-				int distanceTotale = 0;
-				for(int i = 0; i < voisins.size(); i++){
-					distanceTotale += Calculs.distanceChebyshev(possibilites[j], voisins.get((Integer) i));
-				}
-				if(distanceTotale / voisins.size() < meilleureMoyenneDistance){
-					meilleureMoyenneDistance = distanceTotale / voisins.size();
-					meilleurPoint = j;
+
+		int[] distancesTotales = new int[8]; // La distance additionnée entre les ennemis et le personnage en choisissant la case i
+		for(int i = 0; i < 8; i++){
+			if(Calculs.estDansArene(possibilites[i])){
+				Iterator it = voisins.keySet().iterator();
+				while(it.hasNext()){
+					int reference = (int)it.next();
+					if(arene.elementFromRef(reference) instanceof Personnage){
+						distancesTotales[i] += Calculs.distanceChebyshev(possibilites[i], voisins.get((Integer) reference));
+					}
 				}
 			}
 		}
-		return possibilites[meilleurPoint];
+		int meilleurePossibilite = 0;
+		int meilleureDistance = 0;
+		for(int i = 0; i < 8; i++){
+			if(meilleureDistance < distancesTotales[i]){
+				meilleureDistance = distancesTotales[i];
+				meilleurePossibilite = i;
+			}
+		}
+
+		return possibilites[meilleurePossibilite];
 	}
 }
-	

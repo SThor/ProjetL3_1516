adb89e432d37666dfa5dc8abfa2058ba12ed2551
SThor <paulgivel@gmail.com>
2016-01-06 14:28:54 +0100

    fix multilanceur

--- src/lanceur/MultiLanceur.java
+++ src/lanceur/MultiLanceur.java
@@ -6,17 +6,13 @@ public class MultiLanceur {
 	public static void main(String[] args) {
 		//LanceArene lanceArene = new LanceArene();
 		//LanceIHM lanceIHM = new LanceIHM();
-		LancePotion lancepotion;
-		LancePoison lancepoison;
 		//lanceArene.main(null);
 		//lanceIHM.main(null);
 		for(int i = 0; i < 10 ; i++){
-			lancepotion = new LancePotion();
-			lancepotion.main(null);
+			LancePotion.main(null);
 		}
 		for(int i = 0; i < 10 ; i++){
-			lancepoison = new LancePoison();
-			lancepoison.main(null);
+			LancePoison.main(null);
 		}
 	}
 }

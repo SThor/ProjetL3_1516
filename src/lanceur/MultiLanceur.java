4c14a0c2635c5cd11682d8f9d9a8c2253802e226
SThor <paulgivel@gmail.com>
2016-01-06 15:45:28 +0100

    blbl

--- src/lanceur/MultiLanceur.java
+++ src/lanceur/MultiLanceur.java
@@ -1,6 +1,9 @@
 package lanceur;
 
+import lanceur.lanceurPerso.LanceArcher;
+import lanceur.lanceurPerso.LanceFuyard;
 import lanceur.lanceurPerso.LancePoison;
+import lanceur.lanceurPerso.LanceTeleporteur;
 
 public class MultiLanceur {
 	public static void main(String[] args) {
@@ -9,8 +12,11 @@ public class MultiLanceur {
 		for(int i = 0; i < 10 ; i++){
 			LancePotion.main(args);
 		}
-		for(int i = 0; i < 10 ; i++){
+		for(int i = 0; i < 2 ; i++){
 			LancePoison.main(args);
+			LanceArcher.main(args);
+			LanceFuyard.main(args);
+			LanceTeleporteur.main(args);
 		}
 	}
 }

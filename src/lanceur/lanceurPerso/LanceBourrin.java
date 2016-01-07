970f803ee65af6362718bb645d8db086c94075a8
SThor <paulgivel@gmail.com>
2016-01-07 14:20:23 +0100

    lancebourrin

---
+++ src/lanceur/lanceurPerso/LanceBourrin.java
@@ -0,0 +1,97 @@
+package lanceur.lanceurPerso;
+
+import java.awt.Point;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.Random;
+
+import client.strategie.StratBourrin;
+import lanceur.ErreurLancement;
+import logger.LoggerProjet;
+import serveur.element.Caracteristique;
+import serveur.element.personnage.Persons;
+import utilitaires.Calculs;
+import utilitaires.Constantes;
+
+/**
+ * Lance une Console avec un Element sur l'Arene. 
+ * A lancer apres le serveur, eventuellement plusieurs fois.
+ */
+public class LanceBourrin {
+	
+	private static String usage = "USAGE : java " + LanceBourrin.class.getName() + " [ port [ ipArene ] ]";
+
+	public static void main(String[] args) {
+		String nom = "Bourrin";
+		String groupe = "G15"; 
+		
+		// nombre de tours pour ce personnage avant d'etre deconnecte 
+		// (30 minutes par defaut)
+		// si negatif, illimite
+		int nbTours = Constantes.NB_TOURS_PERSONNAGE_DEFAUT;
+		
+		// init des arguments
+		int port = Constantes.PORT_DEFAUT;
+		String ipArene = Constantes.IP_DEFAUT;
+		
+		if (args.length > 0) {
+			if (args[0].equals("--help") || args[0].equals("-h")) {
+				ErreurLancement.aide(usage);
+			}
+			
+			if (args.length > 2) {
+				ErreurLancement.TROP_ARGS.erreur(usage);
+			}
+			
+			try {
+				port = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				ErreurLancement.PORT_NAN.erreur(usage);
+			}
+			
+			if (args.length > 1) {
+				ipArene = args[1];
+			}
+		}
+		
+		// creation du logger
+		LoggerProjet logger = null;
+		try {
+			logger = new LoggerProjet(true, "personnage_" + nom + groupe);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(ErreurLancement.suivant);
+		}
+		
+		// lancement du serveur
+		try {
+			String ipConsole = InetAddress.getLocalHost().getHostAddress();
+			
+			logger.info("Lanceur", "Creation du personnage...");
+			
+			// caracteristiques du personnage
+			HashMap<Caracteristique, Integer> caracts = new HashMap<Caracteristique, Integer>();
+			// seule la force n'a pas sa valeur par defaut (exemple)
+			caracts.put(Caracteristique.FORCE, 
+					Calculs.valeurCaracAleatoire(Caracteristique.FORCE)); 
+			
+			Point position = Calculs.positionAleatoireArene();
+			
+			Persons type;
+			switch (new Random().nextInt(3)){
+				case 1: type = Persons.ELFE; break;
+				case 2: type = Persons.ORKS; break;
+				default: type = Persons.HUMAIN; break;
+			}
+			
+			new StratBourrin(ipArene, port, ipConsole, nom, groupe, caracts, nbTours, position, logger, type);
+			logger.info("Lanceur", "Creation du personnage reussie");
+			
+		} catch (Exception e) {
+			logger.severe("Lanceur", "Erreur lancement :\n" + e.getCause());
+			e.printStackTrace();
+			System.exit(ErreurLancement.suivant);
+		}
+	}
+}
